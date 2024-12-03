9d89e23cbd3259adaf7b140a59ef29be7df51cac
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-12-03 12:25:17 +0000

    Create addToCart function

--- myapp/src/components/Products.js
+++ myapp/src/components/Products.js
@@ -1,9 +1,21 @@
 import '../assets/CSS/layout.css';
+import { useState } from 'react';
+import { flowers } from './FlowerDB';
 import Product from "./Product";
 import Cart from "./Cart";
 
 export default function Products(){
-   
+    const [cart,setCart] = useState([]);
+
+    const addToCart=(product,qty)=>{
+        const existingProduct = cart.find(item=>item.id === product.id);
+        if(existingProduct) {
+            setCart(cart.map(item=>item.id === product.id? {...item, qty: item.qty + qty}:item));
+        } else {
+            setCart([...cart,{...product,qty}])
+        }
+    }
+
     return(
         <>
             <div className="item1">
