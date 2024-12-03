e78d32ceb04556207bb33dae13aa9bb25e0bc6e4
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-12-03 13:06:52 +0000

    Display details in table

--- myapp/src/components/Cart.js
+++ myapp/src/components/Cart.js
@@ -1,5 +1,6 @@
-export default function Cart() {
-    
+export default function Cart({cart}) {
+    const grandTotal = cart.reduce((acc,item)=>acc+item.price*item.qty,0);
+
     return (
         <div className="table-container">
             <h4 className="card-title">Cart</h4>
@@ -12,11 +13,17 @@ export default function Cart() {
 
                 </thead>
                 {
-                    
+                    cart.map(item=>(
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>{item.qty}</td>
+                            <td>{item.price*item.qty}</td>
+                        </tr>
+                    ))
                 }
                 <tr>
                     <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
+                    <td colSpan={2}>{grandTotal}</td>
                 </tr>
             </table>
            
