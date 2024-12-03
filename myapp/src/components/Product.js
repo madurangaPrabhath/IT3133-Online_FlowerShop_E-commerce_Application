82ef9b66f17bf2b42a9fe8b5703fc3f5af2dc835
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-12-03 12:01:50 +0000

    Create structure in Product component

--- myapp/src/components/Product.js
+++ myapp/src/components/Product.js
@@ -1,5 +1,19 @@
-export default function Product() {
+export default function Product(){
+    
     return(
-        <></>
-    )
+        <div className="grid-item">
+
+            <div class="card">
+                <img  />
+                <div class="card-body">
+                    <h5 class="card-title">Price:</h5>
+                    <div class="quantity-container">
+                        <label for="quantity">Quantity:</label>
+                        <input type="number" id="quantity" name="quantity" />
+                    </div>
+                    <button class="card-button">Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    );
 }
