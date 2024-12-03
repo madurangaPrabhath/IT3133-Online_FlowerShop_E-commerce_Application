ad9954d961101c8fef7685ec65f612a203a0fa19
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-12-03 12:03:11 +0000

    Create structure in Cart components

--- myapp/src/components/Cart.js
+++ myapp/src/components/Cart.js
@@ -1,5 +1,25 @@
 export default function Cart() {
-    return(
-        <></>
-    )
+    
+    return (
+        <div className="table-container">
+            <h4 className="card-title">Cart</h4>
+            <table>
+                <thead>
+
+                    <td>Product</td>
+                    <td>QTY</td>
+                    <td>Price</td>
+
+                </thead>
+                {
+                    
+                }
+                <tr>
+                    <td >Grand Total : </td>
+                    <td colSpan={2}>Grand Total</td>
+                </tr>
+            </table>
+           
+        </div>
+    );
 }
