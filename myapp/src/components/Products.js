86590aef0661bb0bdf4f9f9b498f35f67770339a
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-12-03 11:51:42 +0000

    Create Table in Products components to show the images and details

--- myapp/src/components/Products.js
+++ myapp/src/components/Products.js
@@ -1,5 +1,25 @@
-export default function Products() {
+export default function Products(){
+   
     return(
-        <></>
-    )
+        <>
+            <div className="item1">
+                <h1>Flower Shop</h1>
+            </div>
+            <div className="item2">
+                <h4 className="card-title">Buy flowers</h4>
+                <div className="grid-container">
+                    {
+                        //product
+                    }
+                </div>
+
+            </div>
+            <div className="item3">
+                {
+                //cart
+                }
+            </div>
+        </>
+    );
+
 }
